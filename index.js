e3cfa6471c7c2172052fd82d91094e2eab65ab61
Renju Radhakrishnan <renju467@gmail.com>
2017-12-14 13:01:05 -0500

    Added express, created rest API, logging

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-
 const Gdax = require('gdax');
+const express = require('express');
+const bunyan = require('bunyan');
+
+const log = bunyan.createLogger({
+  name: 'algo-trader',
+  // streams: [{
+  //   path: `${__dirname}/algo-trader.log`,
+  // }],
+});
+
+const app = express();
 const secrets = require('./secrets');
 
 const { key, b64secret, passphrase } = secrets;
@@ -7,10 +17,44 @@ const apiURI = 'https://api.gdax.com';
 
 const authedClient = new Gdax.AuthenticatedClient(key, b64secret, passphrase, apiURI);
 
-authedClient.getAccounts((error, response, data) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(data);
-  }
+// Gets account information [BTC, ETH, LTC, USD]
+app.get('/getAccounts', (req, res) => {
+  authedClient.getAccounts((error, response, data) => {
+    if (error) {
+      log.error(error);
+      res.send(error);
+    } else {
+      log.info(data);
+      res.send(data);
+    }
+  });
+});
+
+app.get('/products', (req, res) => {
+  authedClient.getProducts().then((data) => {
+    log.info(data);
+    res.send(data);
+  });
+});
+
+app.get('/orders', (req, res) => {
+  authedClient.getOrders().then((data) => {
+    res.send(data);
+  });
+});
+
+app.get('/trades', (req, res) => {
+  authedClient.getAccountHistory('96f91c70-26a2-44ef-831e-ab0c76ce8a33', (error, response, data) => {
+    if (error) {
+      log.error(error);
+      res.send(error);
+    } else {
+      log.info(data);
+      res.send(data);
+    }
+  });
+});
+
+app.listen(3500, () => {
+  log.info('connected');
 });
