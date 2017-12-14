ec2f65cb75dc31a8dc06af292e02f890b56ce162
Renju Radhakrishnan <renju467@gmail.com>
2017-12-13 21:41:34 -0500

    Using gdax api

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-const crypto = require('crypto');
-const http = require('http');
+
+const Gdax = require('gdax');
 const secrets = require('./secrets');
 
-const { secretKey, passPhrase } = secrets;
+const { key, b64secret, passphrase } = secrets;
+const apiURI = 'https://api.gdax.com';
 
-function buildAccessSign(method, requestPath, body) {
-  const timestamp = Date.now() / 1000;
-  const what = timestamp + method + requestPath + body;
-  const key = Buffer.from(secretKey, 'base64');
-  const hmac = crypto.createHmac('sha256', key);
+const authedClient = new Gdax.AuthenticatedClient(key, b64secret, passphrase, apiURI);
 
-  return hmac.update(what).digest('base64');
-}
-
-function getHeaders(method, requestPath, body) {
-  return {
-    'CB-ACCESS-SIGN': buildAccessSign(method, requestPath, body),
-    'CB-ACCESS-KEY': secretKey,
-    'CB-ACCESS-TIMESTAMP': Date.now(),
-    'CB-ACCESS-PASSPHRASE': passPhrase,
-  };
-}
-
-console.log(getHeaders('GET', '/accounts'));
-
-const options = {
-  port: 1337,
-  hostname: '127.0.0.1',
-  headers: getHeaders('GET', '/accounts'),
-};
-
-// const req = http.request(options);
+authedClient.getAccounts((error, response, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
